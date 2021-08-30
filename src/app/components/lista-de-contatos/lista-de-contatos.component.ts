52a2de7f4cdbb33ba34c630dd60a4c306c2e80ff
Bruna Salom <bruna.salom@gmail.com>
2021-08-30 12:09:36 -0300

    contatos contabilizados mas ainda não exibidos

--- src/app/components/lista-de-contatos/lista-de-contatos.component.ts
+++ src/app/components/lista-de-contatos/lista-de-contatos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contato } from 'src/app/models/Contato';
 
 @Component({
   selector: 'app-lista-de-contatos',
@@ -6,6 +7,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lista-de-contatos.component.css']
 })
 export class ListaDeContatosComponent implements OnInit {
+
+  contatos: Contato[] = [
+    {
+      nome: "Bruna",
+      email: "bruna@teste.com",
+      telefones: ["9999-9999"]
+    }, {
+      nome: "Renan",
+      email: "renan@teste.com",
+      telefones: ["9999-8888"]
+    }, {
+      nome: "Tata",
+      email: "tata@teste.com",
+      telefones: ["9999-7777"]
+    }, {
+      nome: "Gilson",
+      email: "gilson@teste.com",
+      telefones: ["9999-6666"]
+    }, {
+      nome: "Shena",
+      email: "shena@teste.com",
+      telefones: ["9999-5555"]
+    }, {
+      nome: "Madonna",
+      email: "madonna@teste.com",
+      telefones: ["9999-4444"]
+    }, {
+      nome: "Shakira",
+      email: "shakira@teste.com",
+      telefones: ["9999-3333"]
+    }, {
+      nome: "Budweiser",
+      email: "budweiser@teste.com",
+      telefones: ["9999-2222"]
+    }, {
+      nome: "Astor",
+      email: "astor@teste.com",
+      telefones: ["9999-1111"]
+    }, {
+      nome: "Kepler",
+      email: "kepler@teste.com",
+      telefones: ["9999-0000"]
+    }
+  ]
 
   constructor() { }
 
